ab4a45a1cf38620eb91a90ea3f7ba28ed5b11354
Mich Mot <prezesmajkel43@gmail.com>
2021-02-04 16:12:10 +0100

    poprawiono SelectLoadMacroOptionListener

--- src/main/java/edu/kis/powp/jobs2d/events/SelectLoadMacroOptionListener.java
+++ src/main/java/edu/kis/powp/jobs2d/events/SelectLoadMacroOptionListener.java
@@ -1,19 +1,19 @@
 package edu.kis.powp.jobs2d.events;
 
-import edu.kis.powp.jobs2d.command.DriverCommand;
 import edu.kis.powp.jobs2d.command.manager.DriverCommandManager;
+import edu.kis.powp.jobs2d.drivers.MacroDriver;
 import edu.kis.powp.jobs2d.features.CommandsFeature;
 import edu.kis.powp.jobs2d.features.MacroFeature;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 
 public class SelectLoadMacroOptionListener implements ActionListener {
+    MacroDriver macroDriver = MacroFeature.getMacroDriver();
+    DriverCommandManager manager = CommandsFeature.getDriverCommandManager();
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        DriverCommandManager manager = CommandsFeature.getDriverCommandManager();
-        manager.setCurrentCommand(MacroFeature.getMacroDriver().getMacro());
+        manager.setCurrentCommand(macroDriver.getMacro());
     }
 }
